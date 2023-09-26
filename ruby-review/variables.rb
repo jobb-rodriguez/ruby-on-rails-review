15dcb4472c29404afe6f8f1386d57e22c1436f75
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2023-09-26 17:35:43 +0800

    Add: Variables and variable types

---
+++ ruby-review/variables.rb
@@ -0,0 +1,8 @@
+character_name = "John" # string
+character_age = 12 # integer
+character_gpa = 3.2 # float
+character_ismale = true # boolean
+character_money = nil # nil
+
+puts (character_name + " is cool!")
+puts (character_name + " is " + character_age.to_s + " years old.")

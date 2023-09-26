d724a82309b4a68547f1eda17e48a31ee4d55597
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2023-09-26 17:54:25 +0800

    Add: Arrays

---
+++ ruby-review/arrays.rb
@@ -0,0 +1,8 @@
+friends = Array["John", "Nami", "Helia"]
+puts friends[0, 2]
+puts friends.length()
+puts friends.include? "Jonathan"
+puts friends.reverse()
+puts friends.sort()
+
+roles = Array.new

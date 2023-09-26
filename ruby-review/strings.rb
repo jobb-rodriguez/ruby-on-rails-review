f7b15465f8fa7dbe28d10ac7c2b06dd7a4f83133
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2023-09-26 17:37:54 +0800

    Add: String methods

---
+++ ruby-review/strings.rb
@@ -0,0 +1,11 @@
+# String Methods
+phrase = "Love Live!"
+
+puts phrase.upcase()
+puts phrase.downcase()
+puts phrase.strip()
+puts phrase.length()
+puts phrase.include? "Love"
+puts phrase[0]
+puts phrase[0,3]
+puts phrase.index("Live!")

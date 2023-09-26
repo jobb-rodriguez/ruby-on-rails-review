11f59b7d802e895f9759f67fb6def6bbf4932d37
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2023-09-26 20:48:23 +0800

    Add: Control FLow

---
+++ ruby-review/control_flow.rb
@@ -0,0 +1,21 @@
+# if-elsif-else end
+# and, or, !(not)
+# >, >=, <, <=, ==, !=
+
+# switch
+# case when-else end
+
+# while-expression-end
+
+# for x in container end
+# container.each do |x| expression end
+
+for index in 0..5 # inclusive
+    puts index
+end
+
+# 6.times do |index| expression end
+
+=begin
+    multi-line comments
+=end
